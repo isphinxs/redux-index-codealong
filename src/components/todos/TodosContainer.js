43d7a2cfda876625f09905b201e72334200eea09
Samantha Ostrowski <isphinxs@hotmail.com>
2021-08-15 19:07:08 -0400

    Done.

--- src/components/todos/TodosContainer.js
+++ src/components/todos/TodosContainer.js
@@ -4,7 +4,7 @@ import Todo from './Todo';
 
 class TodosContainer extends Component {
 
-  renderTodos = () => this.props.todos.mapStateToProps((todo, id) => <Todo key={id} text={todo} />)
+  renderTodos = () => this.props.todos.map((todo, id) => <Todo key={id} text={todo} />)
 
   render() {
     console.log("Todos state: ", this.props.todos);
